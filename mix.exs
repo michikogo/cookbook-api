91fa0e21700c5f31c88c18797575d3722b547d1b
Michiko Go <michiko.go@podium.com>
2023-05-29 00:36:18 -0400

    add argon - hashing package

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule CookbookApi.MixProject do
       {:absinthe, "~> 1.7"},
       {:absinthe_plug, "~> 1.5"},
       {:absinthe_phoenix, "~> 2.0"},
+      {:argon2_elixir, "~> 2.2"},
       {:phoenix, "~> 1.5.13"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.4"},

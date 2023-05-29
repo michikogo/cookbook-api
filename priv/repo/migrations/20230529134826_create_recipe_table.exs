a919d9121fd822d071b32630d632ca60fa26556d
Michiko Go <michiko.go@podium.com>
2023-05-29 10:16:34 -0400

    create user table

---
+++ priv/repo/migrations/20230529134826_create_recipe_table.exs
@@ -0,0 +1,29 @@
+defmodule CookbookApi.Repo.Migrations.CreateRecipeTable do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
+
+  def change do
+    create table(:recipes) do
+      add(:uid, :uuid, default: fragment("gen_random_uuid()"), null: false, primary_key: true)
+
+      add(:title, :string, null: false)
+      add(:description, :string, null: false)
+
+      add(:ingredients, {:array, :string}, null: false)
+      add(:instructions, {:array, :text}, null: false)
+
+      add(:prep_time, :string, null: false)
+      add(:cook_time, :string, null: false)
+      add(:total_time, :string)
+
+      add(:tags, {:array, :string})
+
+      timestamps()
+    end
+
+    create(
+      unique_index(:recipes, [:title, :uid], name: :xid_recipe_uid_title, concurrently: true)
+    )
+  end
+end

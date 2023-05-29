1bb759491cd4bdd6bc96a4f86672ba5195243134
Michiko Go <michiko.go@podium.com>
2023-05-28 22:34:33 -0400

    define user schema in project

---
+++ lib/cookbook_api/Users/user.ex
@@ -0,0 +1,41 @@
+defmodule CookbookApi.Users.User do
+  @moduledoc """
+  The User schema.
+  """
+
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @required_fields [
+    :first_name,
+    :last_name,
+    :email,
+    :password
+  ]
+
+  @fields @required_fields ++
+            [
+              :uid,
+              :recipe_ids
+            ]
+
+  schema "users" do
+    field(:uid, Ecto.UUID, primary_key: true)
+
+    field(:first_name, :string)
+    field(:last_name, :string)
+    field(:email, :string)
+    field(:password, :string)
+
+    field(:recipe_ids, {:array, :string})
+
+    timestamps()
+  end
+
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, @fields)
+    |> validate_required(@required_fields)
+  end
+end

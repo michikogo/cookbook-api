915c5e92de4d47e8046afc8db5a41cf38e5ab553
Michiko Go <michiko.go@podium.com>
2023-05-29 00:00:45 -0400

    create endpoint to fetch all users

--- lib/cookbook_api_web/views/schema/users/resolvers.ex
+++ lib/cookbook_api_web/views/schema/users/resolvers.ex
@@ -1,5 +1,15 @@
 defmodule CookbookApiWeb.Schema.Users.Resolvers do
+  @moduledoc """
+  This module contains the resolvers for the users schema.
+  """
+
+  alias CookbookApi.Users
+
   def initial(_args, _info) do
     {:ok, "THE ENDPOINT WORKS!"}
+  end
+
+  def get_all_users(_args, _info) do
+    Users.get_all_users()
   end
 end
